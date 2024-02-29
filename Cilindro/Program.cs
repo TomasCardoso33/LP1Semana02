50f694112d478c5fd780d1dc38aeff88ce6aa93f
Tomás <tomascardoso.dev@gmail.com>
2024-02-29 21:31:22 +0000

    Added 2 notes to explain what the code does in lines 15 and 18

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -12,10 +12,10 @@ namespace Cilindro
         Console.Write("Digite o raio do cilindro: ");
         float r = MathF.Abs(float.Parse(Console.ReadLine())); 
 
-        
+        // Calcula o volume 
         float v = MathF.PI * MathF.Pow(r, 2) * a;
 
-        
+        // Calcula a área de superfície 
         float aS = 2 * MathF.PI * r * (r + a);
 
         Console.WriteLine($"Volume do cilindro: {v}");
