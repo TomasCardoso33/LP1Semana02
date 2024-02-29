9f2f487c13a9a315f73d9af8727cd4144e157df1
Tomás <tomascardoso.dev@gmail.com>
2024-02-29 21:20:24 +0000

    Added 4 new operations, Division, shift by 3, Xor with 5 and check if its bigger than 10

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -9,8 +9,21 @@ namespace SmoothOperator
             Console.WriteLine("Escreva um número inteiro não negativo");
             string s1 = Console.ReadLine();
             byte b1 = byte.Parse(s1);
+            // Decrementar
             Console.WriteLine($"{--b1}");
+            //Incrementar
             Console.WriteLine($"{++b1}");
+             // Divisão por 2
+            Console.WriteLine($"{b1 / 2}");
+
+            // Shift left por 3
+            Console.WriteLine($"{b1 << 3}");
+
+            // Xor com 5
+            Console.WriteLine($"{b1 ^ 5}");
+
+            // Se é maior que 10
+            Console.WriteLine($"{b1 > 10}");
             
             
         }
