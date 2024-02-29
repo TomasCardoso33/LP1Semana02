b2ee11921287d22b28b161a72f1ca5144f54da44
Tomás <tomascardoso.dev@gmail.com>
2024-02-29 21:09:03 +0000

    Added incrementation

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número inteiro não negativo");
+            string s1 = Console.ReadLine();
+            byte b1 = byte.Parse(s1);
+            Console.WriteLine($"{--b1}");
+            Console.WriteLine($"{++b1}");
+            
+            
+        }
+    }
+}
