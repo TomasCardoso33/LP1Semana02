d561bcaea3932179e14e22eb5966e419855863e9
Tomás <tomascardoso.dev@gmail.com>
2024-02-29 22:07:58 +0000

    Added max and min values of the various types learned in LP1

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        Console.WriteLine($"sbyte: {sbyte.MaxValue}, {sbyte.MinValue}");
+        Console.WriteLine($"byte: {byte.MaxValue}, {byte.MinValue}");
+        Console.WriteLine($"short: {short.MaxValue}, {short.MinValue}");
+        Console.WriteLine($"ushort: {ushort.MaxValue}, {ushort.MinValue}");
+        Console.WriteLine($"int: {int.MaxValue}, {int.MinValue}");
+        Console.WriteLine($"uint: {uint.MaxValue}, {uint.MinValue}");
+        Console.WriteLine($"long: {long.MaxValue}, {long.MinValue}");
+        Console.WriteLine($"ulong: {ulong.MaxValue}, {ulong.MinValue}");
+        Console.WriteLine($"float: {float.MaxValue}, {float.MinValue}");
+        Console.WriteLine($"double: {double.MaxValue}, {double.MinValue}");
+        Console.WriteLine($"decimal: {decimal.MaxValue}, {decimal.MinValue}");
+        }
+    }
+}
