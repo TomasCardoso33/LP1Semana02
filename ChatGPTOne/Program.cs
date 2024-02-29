e81ab96c77daa3bfca46cf195694d4ebd3866a0f
Tomás <tomascardoso.dev@gmail.com>
2024-02-29 22:57:04 +0000

    Added first question (Who are you?)

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string question, response;
+
+            do
+            {
+                Console.Write("Ask and i shall answer? (or say EXIT to quit) ");
+                question = Console.ReadLine();
+
+
+                switch (question)
+                {
+                    case "Who are you?":
+                        response = "Im the one who knocks!";
+                        break;
+                    case "What's your name?":
+                        response = "Gran'pa";
+                        break;
+                    case "EXIT":
+                        response = "Ill be back";
+                        break;
+                    case  "Are you more intelligent than me?":
+                        response = "Obviously!";
+                        break;
+                    default:
+                        response = "You got me, I'm not THAT smart!";
+                        break;
+                }
+
+                // Responder adequadamente
+                Console.WriteLine(response);
+            } while (true);
+        }
+    }
+}
