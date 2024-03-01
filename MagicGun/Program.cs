5abd94da572acd786df85c43c9802f108c7579c5
TomasCardoso33 <tomascardoso.dev@gmail.com>
2024-03-01 16:24:08 +0000

    Converted variable from string to int

---
+++ MagicGun/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MagicGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um número inteiro:  ");
+            string s1 = Console.ReadLine();
+            int i1 = int.Parse(s1);
+            Console.WriteLine(i1);
+
+
+
+
+        }
+    }
+}
